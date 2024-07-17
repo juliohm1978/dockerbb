61b26a2ac4de5f1a4b892b0219f9f0dbdcfc9595
Keny <keny.oshiro92@gmail.com>
2024-07-17 22:22:18 +0000

    Update epoint.sh

--- rootfs/usr/local/bin/epoint.sh
+++ rootfs/usr/local/bin/epoint.sh
@@ -1,16 +1,6 @@
 #!/bin/bash -xe
 
-if ! getent group $USER_GID >/dev/null; then
-    groupadd -g $USER_GID user
-else
-    USER_GID=$((USER_GID + 1000))
-    groupadd -g $USER_GID user
-fi
-if ! getent passwd $USER_UID >/dev/null; then
-    useradd -u $USER_UID -g $USER_GID -ms /bin/bash user
-else
-    useradd -u $((USER_UID + 1000)) -g $USER_GID -ms /bin/bash user
-fi
+
 chown -R user.user /home/user
 
 exec "$@"
