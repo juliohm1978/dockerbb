7ac67cb3bffce136fe24e328ffac8c20e2fc833e
Keny <keny.oshiro92@gmail.com>
2024-07-17 22:49:20 +0000

    Update startbrowser.sh

--- rootfs/usr/local/bin/startbrowser.sh
+++ rootfs/usr/local/bin/startbrowser.sh
@@ -3,4 +3,4 @@
 /usr/bin/dpkg -D10 -i /w.deb
 service warsaw restart
 
-runuser -l user -c "DISPLAY=:20 /usr/bin/chromium-browser --disable-dev-shm-usage --start-maximized $CHROMIUM_URL"
+runuser -l user -c "DISPLAY=:20 /usr/bin/chromium-browser --disable-dev-shm-usage --start-maximized https://www.bb.com.br"
