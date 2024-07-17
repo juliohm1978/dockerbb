67023f55797d238a412204c5caa63bced8757494
Keny <keny.oshiro92@gmail.com>
2024-07-17 23:57:32 +0000

    Update startbrowser.sh

--- rootfs/usr/local/bin/startbrowser.sh
+++ rootfs/usr/local/bin/startbrowser.sh
@@ -1,11 +1,10 @@
 #!/bin/bash -xe
 
-/usr/bin/dpkg -D10 -i /w.deb
-service warsaw restart
-
-
 if [ -f /home/user/.config/chromium/SingletonLock ]; then
     rm /home/user/.config/chromium/SingletonLock
 fi
+
+/usr/bin/dpkg -D10 -i /w.deb
+service warsaw restart
 
 runuser -l user -c "DISPLAY=:20 /usr/bin/chromium-browser --disable-dev-shm-usage --start-maximized https://www.bb.com.br"
