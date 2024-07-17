88d38d633a86b20d0938e20590fa4d9f7ef96953
Keny <keny.oshiro92@gmail.com>
2024-07-17 22:15:37 +0000

    Update epoint.sh

--- rootfs/usr/local/bin/epoint.sh
+++ rootfs/usr/local/bin/epoint.sh
@@ -3,20 +3,13 @@
 if ! getent group $USER_GID >/dev/null; then
     groupadd -g $USER_GID user
 else
-    while getent group $USER_GID >/dev/null; do
-        USER_GID=$((USER_GID + 1))
-    done
+    USER_GID=$((USER_GID + 1000))
     groupadd -g $USER_GID user
 fi
-
-# Check if the user with USER_UID exists
 if ! getent passwd $USER_UID >/dev/null; then
     useradd -u $USER_UID -g $USER_GID -ms /bin/bash user
 else
-    while getent passwd $USER_UID >/dev/null; do
-        USER_UID=$((USER_UID + 1))
-    done
-    useradd -u $USER_UID -g $USER_GID -ms /bin/bash user
+    useradd -u $((USER_UID + 1000)) -g $USER_GID -ms /bin/bash user
 fi
 chown -R user.user /home/user
 
